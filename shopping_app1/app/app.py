1575e3500d84bfaeadfaefe68f6878ce7199e766
Alex Mochu <mochualex4@gmail.com>
2017-08-30 08:34:17 +0300

    Initilized the database

--- shopping_app1/app/app.py
+++ shopping_app1/app/app.py
@@ -20,6 +20,17 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print('Initilized the database')
+
 def get_db():
 
     if not hasattr(g, 'sqlite_db'):
