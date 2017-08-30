35ecfd49677a7062532b5d02f9702cc105e834e2
Alex Mochu <mochualex4@gmail.com>
2017-08-31 00:36:28 +0300

    Created the registration form  view

---
+++ designs/shopping_designs_app/designs_app/auth/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+
+from ..models import Employee
+
+class RegistrationForm(FlaskForm):
+    
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+                                        DataRequired(),
+                                        EqualTo('confirm_password')
+                                        ])
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Register')
