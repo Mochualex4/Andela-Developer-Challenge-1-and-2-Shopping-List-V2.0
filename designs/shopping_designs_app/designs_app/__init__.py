5797877b5b73499816f753a8ee0a8688679cf9ba
Alex Mochu <mochualex4@gmail.com>
2017-09-04 16:11:22 +0300

    Added Login Form with auth

--- designs/shopping_designs_app/designs_app/__init__.py
+++ designs/shopping_designs_app/designs_app/__init__.py
@@ -1,1 +1,2 @@
 from .designs_app import app
+
--- designs/shopping_designs_app/designs_app/designs_app.py
+++ designs/shopping_designs_app/designs_app/designs_app.py
@@ -7,7 +7,6 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def index():
-
     return render_template("homepage.html")
 
 @app.route('/signup')
@@ -17,10 +16,16 @@ def register():
 
 @app.route('/dashboard')
 def dashboard():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template("dashboard.html")
 
-    return render_template("dashboard.html")
-
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return render_template('login.html')
     
-    return render_template("login.html")
